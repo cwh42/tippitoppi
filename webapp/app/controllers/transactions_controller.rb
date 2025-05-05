792f70d0c5a919e7445d31c83bceb9d90a5c4f9b
Christopher Hofmann <cwh@suse.de>
2025-05-05 16:44:24 +0200

    tip_percent for relations; only sync today

--- webapp/app/controllers/transactions_controller.rb
+++ webapp/app/controllers/transactions_controller.rb
@@ -1,6 +1,7 @@
 class TransactionsController < ApplicationController
   def index
     @date = params[:date]&.to_date || Time.zone.today
-    @transactions = Transaction.sync(@date)
+    Transaction.sync if @date.today? # FIXME: sync if day not synced
+    @transactions = Transaction.date(@date)
   end
 end
--- webapp/app/models/transaction.rb
+++ webapp/app/models/transaction.rb
@@ -2,9 +2,16 @@ class Transaction < ApplicationRecord
   validates :upstream_id, uniqueness: true
   validates :upstream_id, :amount, :tip_amount, :timestamp, presence: true
 
-  scope :today, -> { where(timestamp: Time.zone.now.beginning_of_workday..Time.zone.now.end_of_workday) }
+  scope :date, ->(date) { where(timestamp: date&.to_date.beginning_of_workday..date&.to_date.end_of_workday) }
+  scope :today, -> { date(Time.zone.today) }
 
   def tip_percent
+    tip_amount/(amount - tip_amount) * 100
+  end
+
+  def self.tip_percent
+    amount = sum { |t| t.amount }
+    tip_amount = sum { |t| t.tip_amount }
     tip_amount/(amount - tip_amount) * 100
   end
 
