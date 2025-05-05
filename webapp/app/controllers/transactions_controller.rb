552afedf98f68e7c8980441e73b8474b52f00536
Christopher Hofmann <cwh@suse.de>
2025-05-05 16:12:18 +0200

    Show weekday

--- webapp/app/controllers/transactions_controller.rb
+++ webapp/app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionsController < ApplicationController
   def index
-    @date = params[:date].to_date || Time.zone.today
+    @date = params[:date]&.to_date || Time.zone.today
     @transactions = Transaction.sync(@date)
   end
 end
