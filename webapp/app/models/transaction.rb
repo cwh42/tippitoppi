a65030c00ecabb770995cac597c565fdddff9e42
Christopher Hofmann <cwh@suse.de>
2025-05-07 12:02:51 +0200

    Add .share for relation

--- webapp/app/models/transaction.rb
+++ webapp/app/models/transaction.rb
@@ -3,10 +3,24 @@ class Transaction < ApplicationRecord
   validates :upstream_id, :amount, :tip_amount, :timestamp, presence: true
 
   scope :at_date, ->(date) { where(timestamp: date&.to_date.beginning_of_workday..date&.to_date.end_of_workday) }
+  scope :yesterday, -> { at_date(Time.zone.yesterday) }
   scope :today, -> { at_date(Time.zone.today) }
 
   def workers
     Worktime.at_time(timestamp)
+  end
+
+  def share
+    tip = tip_amount / workers.size
+    workers.to_h {|w| [w.name, tip] }
+  end
+
+  def self.share
+    all.each_with_object(Hash.new(0)) do |t, hash|
+      t.share.each_with_object(hash) do |(k, v), hash|
+        hash[k] += v
+      end
+    end
   end
 
   def tip_percent
