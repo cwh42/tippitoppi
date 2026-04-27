c636dd12ae413afd41a5dd69d71f0806dfae99c6
Christopher Hofmann <cwh@suse.de>
2026-04-27 17:17:18 +0200

    Do not sync transactions by just watching them

--- webapp/app/controllers/transactions_controller.rb
+++ webapp/app/controllers/transactions_controller.rb
@@ -1,7 +1,7 @@
 class TransactionsController < ApplicationController
   def index
     @date = params[:date]&.to_date || Time.zone.today
-    Transaction.sync if @date.today? # FIXME: sync if day not synced
+    #Transaction.sync if @date.today? # FIXME: sync if day not synced
     @transactions = Transaction.at_date(@date)
   end
 end
